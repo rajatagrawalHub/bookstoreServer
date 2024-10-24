9ea812ba35e39c4dbb93837769f7d51684b61ea1
rajatAgrawalHub <rajat.agrawal2022@vitstudent.ac.in>
2024-10-25 02:31:53 +0530

    Updated Security Commit

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const express = require('express')
 const {ConnectToDb, getDb} = require('./db')
 const cors = require('cors')
 const {ObjectId} = require('mongodb')
+const bcrypt = require('bcrypt');
+const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
+const axios = require('axios');
 
 const app = express()
 
@@ -10,6 +14,10 @@ app.use(express.json())
 app.use(cors({
     origin: 'http://localhost:3000'
 }));
+
+let otps = {};
+
+app.use(bodyParser.json());
 
 let db
 
@@ -119,7 +127,7 @@ app.get('/getToken/:tokenID',(req,res)=>{
 
 app.get('/fetchUser/:id',(req,res)=>{
     db.collection('Auth')
-    .findOne({_id: req.params.id})
+    .findOne({_id: new ObjectId(req.params.id)})
     .then((auth)=>{res.status(200).json(auth)})
     .catch(()=>{res.status(500).json({error: "Could Not Auth the transaction"})})
 })
@@ -132,3 +140,90 @@ app.post('/addBook',(req,res)=>{
     .then((auth)=>{res.status(200).json(auth)})
     .catch(()=>{res.status(500).json({error: "Could Not Insert the Book"})})
 })
+
+app.post('/addUser',async (req,res)=>{
+    let newUserObj = req.body
+    newUserObj.password = await bcrypt.hash(newUserObj.password,10)
+    db.collection('Auth')
+    .insertOne(newUserObj)
+    .then((auth)=>{res.status(200).json(auth)})
+    .catch(()=>{res.status(500).json({error: "Could Not Create the User"})})
+})
+
+app.post('/compare',async (req,res)=>{
+    let passwordObjects = req.body
+    let passwordentered = passwordObjects.pEntered;
+    let passwordfetched = passwordObjects.pFetched;
+    
+    if(await bcrypt.compare(passwordentered,passwordfetched) == true){
+        res.status(200).json({status: true})
+    }else{
+        res.status(500).json({status: false})
+    }
+})
+
+app.post('/sendOtp', (req, res) => {
+    const { email } = req.body;
+    
+    const otp = Math.floor(100000 + Math.random() * 900000).toString();
+
+    otps[email] = otp;
+
+    const transporter = nodemailer.createTransport({
+        service: 'Gmail',
+        auth: {
+            user: 'rajatagrawal300@gmail.com',
+            pass: 'nndy rmfj vevw ruwd',
+        },
+    });
+
+    const mailOptions = {
+        from: 'rajatagrawal300@gmail.com',
+        to: email,
+        subject: 'Your OTP Code',
+        text: `Your OTP is: ${otp}`,
+    };
+
+    console.log(transporter);
+    console.log(mailOptions);
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return res.status(500).json({ success: false, error: error.message, othererror: error.cause });
+        }
+        res.status(200).json({ success: true });
+    });
+});
+
+app.post('/verifyOtp', (req, res) => {
+    const { email, otp } = req.body;
+
+    if (otps[email] && otps[email] === otp) {
+        delete otps[email];
+        res.status(200).json({ success: true });
+    } else {
+        res.status(401).json({ success: false });
+    }
+});
+
+app.post('/verifyCaptcha', async (req, res) => {
+    const { captchaToken } = req.body;
+
+    try {
+        const response = await axios.post(`https://www.google.com/recaptcha/api/siteverify`, null, {
+            params: {
+                secret: '6LeOSWsqAAAAAAyoRGa5s7T9mUiO2d0f5bU2a3Je', 
+                response: captchaToken,
+            },
+        });
+
+        if (response.data.success) {
+            res.status(200).json({ success: true });
+        } else {
+            res.status(400).json({ success: false, message: "CAPTCHA verification failed" });
+        }
+    } catch (error) {
+        console.error("Error verifying CAPTCHA:", error);
+        res.status(500).json({ success: false, error: "Server error" });
+    }
+});
