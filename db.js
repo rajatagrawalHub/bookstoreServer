8e9468639fa6b2a5d494557e3c04d37bd85a2089
rajatAgrawalHub <rajat.agrawal2022@vitstudent.ac.in>
2024-10-25 02:37:03 +0530

    Updated Connection String

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ let dBConnection;
 
 module.exports = {
     ConnectToDb : (callBackFn)=>{
-        MongoClient.connect("mongodb://localhost:27017/BookNExt")
+        MongoClient.connect("mongodb+srv://dbUser01:k1fWY3Ty3CS0Og0q@mainclusterm0.wec8a.mongodb.net/BookNExt")
         .then((client)=>{
             dBConnection = client.db()
             return callBackFn()
